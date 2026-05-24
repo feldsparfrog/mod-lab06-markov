2aac3b4872e21d27a7a57886e731078add7d7085
feldsparfrog <schepalovnickolay@gmail.com>
2026-05-25 02:44:18 +0300

    Update AllTests.cpp

--- test/AllTests.cpp
+++ test/AllTests.cpp
@@ -1,7 +1,7 @@
 // Copyright 2020 GHA Test Team
 
 #include <gtest/gtest.h>
-#include "pch.h"
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
