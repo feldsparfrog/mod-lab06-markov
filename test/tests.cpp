1ab82550188ca6a02719d4f90a23855ea1c6f782
feldsparfrog <schepalovnickolay@gmail.com>
2026-05-25 04:39:06 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,20 +4,20 @@
 #include <string>
 #include <vector>
 #include "textgen.h"
-TEST(Tests, TestPrefixSize) {
+static TEST(Tests, TestPrefixSize) {
     setlocale(LC_ALL, "Russian");
     std::vector<std::string> testsentence =
     { "Съешь", "же", "этих", "французских", "булок", "да", "выпей", "чаю" };
     EXPECT_EQ(CreatePrefix(testsentence, 3, 0).size(), 3);
 }
-TEST(Tests, TestPrefix) {
+static TEST(Tests, TestPrefix) {
     setlocale(LC_ALL, "Russian");
     std::vector<std::string> testsentence =
     { "Съешь", "же", "этих", "французских", "булок", "да", "выпей", "чаю" };
     prefix testprefix = { "этих", "французских", "булок" };
     EXPECT_EQ(CreatePrefix(testsentence, 3, 2), testprefix);
 }
-TEST(Tests, TestSuffixSize) {
+static TEST(Tests, TestSuffixSize) {
     setlocale(LC_ALL, "Russian");
     std::vector<std::string> testsentence =
     { "ты", "мне", "ворон,", "ты", "мне", "воздух,", "величиною", "с", "кулак",
@@ -25,7 +25,7 @@ TEST(Tests, TestSuffixSize) {
     prefix testprefix = CreatePrefix(testsentence, 2, 0);
     EXPECT_EQ(CreateSuffix(testsentence, 2, testprefix).size(), 2);
 }
-TEST(Tests, TestSuffix) {
+static TEST(Tests, TestSuffix) {
     setlocale(LC_ALL, "Russian");
     std::vector<std::string> testsentence =
     { "ты", "мне", "ворон,", "ты", "мне", "воздух,", "величиною", "с", "кулак",
@@ -34,7 +34,7 @@ TEST(Tests, TestSuffix) {
     std::vector<std::string> testsuffix = { "ворон,", "воздух," };
     EXPECT_EQ(CreateSuffix(testsentence, 2, testprefix), testsuffix);
 }
-TEST(Tests, TestTable) {
+static TEST(Tests, TestTable) {
     setlocale(LC_ALL, "Russian");
     std::vector<std::string> testsentence =
     { "ты", "мне", "ворон,", "ты", "мне", "воздух,", "величиною", "с", "кулак",
@@ -45,11 +45,11 @@ TEST(Tests, TestTable) {
     std::vector<std::string> testsuffix = { "ворон,", "воздух," };
     EXPECT_EQ(testtab[testprefix], testsuffix);
 }
-TEST(Tests, TestRandomNumber) {
+static TEST(Tests, TestRandomNumber) {
     int testgen = RandomNumber(20);
     EXPECT_TRUE(testgen >= 0 && testgen <= 20);
 }
-TEST(Tests, TestPrefixGenerate) {
+static TEST(Tests, TestPrefixGenerate) {
     setlocale(LC_ALL, "Russian");
     std::vector<std::string> testsentence =
     { "Съешь", "же", "этих", "французских", "булок", "да", "выпей", "чаю" };
@@ -60,7 +60,7 @@ TEST(Tests, TestPrefixGenerate) {
         GeneratePrefix(testtab).back() == "выпей" ||
         GeneratePrefix(testtab).back() == "чаю");
 }
-TEST(Tests, TestSuffixChoose1) {
+static TEST(Tests, TestSuffixChoose1) {
     setlocale(LC_ALL, "Russian");
     std::vector<std::string> testsentence =
     { "ты", "мне", "ворон,", "ты", "мне", "воздух,", "величиною", "с", "кулак",
@@ -70,7 +70,7 @@ TEST(Tests, TestSuffixChoose1) {
         CreateTable(testsentence, 2);
     EXPECT_EQ(ChooseSuffix(testtab, testprefix), "ты");
 }
-TEST(Tests, TestSuffixChoose2) {
+static TEST(Tests, TestSuffixChoose2) {
     setlocale(LC_ALL, "Russian");
     std::vector<std::string> testsentence =
     { "ты", "мне", "ворон,", "ты", "мне", "воздух,", "величиною", "с", "кулак",
@@ -81,7 +81,7 @@ TEST(Tests, TestSuffixChoose2) {
     EXPECT_TRUE(ChooseSuffix(testtab, testprefix) == "ворон," ||
         ChooseSuffix(testtab, testprefix) == "воздух,");
 }
-TEST(Tests, TestGenerate) {
+static TEST(Tests, TestGenerate) {
     setlocale(LC_ALL, "Russian");
     std::vector<std::string> testsentence =
     { "ты", "мне", "ворон,", "ты", "мне", "воздух,", "величиною", "с", "кулак",
