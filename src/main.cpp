e22ddfbe7bf65986d5a14cd6b267556baa7b3eea
feldsparfrog <schepalovnickolay@gmail.com>
2026-05-25 03:11:02 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 // Copyright 2021 GHA Test Team
-#include "textgen.h"
+#include <map>
 #include <string>
 #include <vector>
+#include "textgen.h"
 int main() {
     std::vector<std::string> allwords = ReadFromFile("training.txt");
-    std::map<prefix, std::vector<std::string>> statetab = 
+    std::map<prefix, std::vector<std::string>> statetab =
         CreateTable(allwords, NPREF);
     Generate(statetab, MAXGEN);
 }
